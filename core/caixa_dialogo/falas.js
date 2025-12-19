3462560c07b969b0912863009d31929d56fead7b
Myiabi <nyaa7@hotmail.com>
2025-12-19 19:42:36 -0300

    Responsividade do jardim e Bar completos. Tela final responsiva.

--- core/caixa_dialogo/falas.js
+++ core/caixa_dialogo/falas.js
@@ -574,10 +574,7 @@ window.personagens = {
         {
           texto:
             "Visit Wayway's shop near the temple. He is very helpful. If you need anything, he will certainly have it.",
-            executar: function () {
-            mudarCenario(personagens.lily, "jardim");
-            mudarCenario(personagens.aiko, "boss3");
-          },
+            
           expressao: "sorrindo",
         },
       ],
